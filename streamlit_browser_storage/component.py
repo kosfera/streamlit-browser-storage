0139fcca54c12f496f62ccfc58cda9a2feaf282c
Maciej <964938+maciejATcosphere@users.noreply.github.com>
2022-07-10 15:43:51 +0200

    fixes

--- streamlit_browser_storage/component.py
+++ streamlit_browser_storage/component.py
@@ -2,12 +2,10 @@ from pathlib import Path
 
 import streamlit.components.v1 as components
 
-IS_RELEASE = False
+frontend_build = Path(__file__).parent / "frontend/build"
 
-if IS_RELEASE:
-    component = components.declare_component(
-        "browser_storage", path=Path(__file__).parent / "frontend/build"
-    )
+if frontend_build.exists():
+    component = components.declare_component("browser_storage", path=frontend_build)
 
 else:
     component = components.declare_component("browser_storage", url="http://localhost:3000")
